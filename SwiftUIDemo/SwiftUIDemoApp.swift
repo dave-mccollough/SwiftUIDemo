4cb60b9d0c8245ed5a5610dbb77b1727522374af
Dave McCollough <davemccollough@gmail.com>
2025-09-05 09:22:36 -0600

    Initial Commit

---
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDemoApp.swift
+//  SwiftUIDemo
+//
+//  Created by Dave McCollough on 9/5/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
