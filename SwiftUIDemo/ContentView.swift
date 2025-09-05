ac48c6f4b3c715dd02952d1ba67e0613cdd31607
Dave McCollough <davemccollough@gmail.com>
2025-09-05 10:22:57 -0600

    SwiftUI demo project

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -9,13 +9,60 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+
+        ZStack {
+            Color(.systemMint)
+                .ignoresSafeArea(edges: .all)
+
+            VStack(alignment: .leading, spacing: 20.0) {
+                Image("elbert")
+                    .resizable()
+                    .cornerRadius(10)
+                    .aspectRatio(contentMode: .fit)
+
+                HStack {
+                    Text("Mount Elbert")
+                        .font(.title)
+                        .fontWeight(.bold)
+
+                    Spacer()
+
+                    VStack {
+                        HStack {
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.fill")
+                            Image(systemName: "star.leadinghalf.filled")
+                        }
+
+                        Text("Reviews 3,567")
+                    }
+                    .foregroundColor(.orange)
+                    .font(.caption)
+                }
+                Text("Colorado's Highest Peak")
+
+                HStack {
+                    Spacer()
+                    Image(systemName: "fork.knife")
+                    Image(systemName: "binoculars.fill")
+
+                }
+                .foregroundColor(.gray)
+                .font(.caption)
+
+            }
+            .padding()
+            .background(
+                Rectangle()
+                    .foregroundColor(.white)
+                    .cornerRadius(15)
+                    .shadow(radius: 15)
+            )
+            .padding()
+
         }
-        .padding()
     }
 }
 
